cb9b5323f532987eaa29054b8f8b2f452e931503
MozzammelRidoy <mozzammelridoy5iii@gmail.com>
2025-05-17 18:18:59 +0600

    use try catch and set success formate

--- src/app/modules/User/user_contoller.ts
+++ src/app/modules/User/user_contoller.ts
@@ -3,9 +3,21 @@ import { UserServices } from "./user_service";
 
 // crate admin.
 const createAdmin = async (req: Request, res: Response) => {
-  //   console.log(req.body);
-  const result = await UserServices.createAdmin(req.body);
-  res.send(result);
+  try {
+    //   console.log(req.body);
+    const result = await UserServices.createAdmin(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin created successfully",
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || "something went wrong",
+      error: err,
+    });
+  }
 };
 
 export const UserControllers = {
