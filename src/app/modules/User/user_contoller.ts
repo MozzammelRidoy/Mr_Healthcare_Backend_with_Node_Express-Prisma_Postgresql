ea9c30f820df0da257c7a05b10956a4c7be14e40
MozzammelRidoy <mozzammelridoy5iii@gmail.com>
2025-05-17 12:48:53 +0600

    update schema and set default value

--- src/app/modules/User/user_contoller.ts
+++ src/app/modules/User/user_contoller.ts
@@ -3,6 +3,7 @@ import { UserServices } from "./user_service";
 
 // crate admin.
 const createAdmin = async (req: Request, res: Response) => {
+  console.log(req.body);
   const result = await UserServices.createAdmin();
   res.send(result);
 };
--- src/app/modules/User/user_route.ts
+++ src/app/modules/User/user_route.ts
@@ -3,6 +3,6 @@ import { UserControllers } from "./user_contoller";
 
 const router = express.Router();
 
-router.get("/", UserControllers.createAdmin);
+router.post("/", UserControllers.createAdmin);
 
 export const UserRoutes = router;
