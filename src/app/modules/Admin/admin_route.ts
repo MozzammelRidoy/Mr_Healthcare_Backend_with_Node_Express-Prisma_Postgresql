5f034d4edb7dda9e7554ebfd3f200465242130a9
MozzammelRidoy <mozzammelridoy5iii@gmail.com>
2025-05-22 23:18:22 +0600

    install zod pakage and setup validateRequiest Middleware for inpurt validation

--- src/app/modules/Admin/admin_route.ts
+++ src/app/modules/Admin/admin_route.ts
@@ -1,10 +1,32 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { AdminControllers } from "./admin_controller";
+import { AnyZodObject, z } from "zod";
 
+const updateDataValidationZodScheam = z.object({
+  body: z.object({
+    name: z.string(),
+    contactNumber: z.string(),
+  }),
+});
+
+const validateRequest =
+  (schema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({ body: req.body });
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
 const router = express.Router();
 router.get("/", AdminControllers.getAllAdmins);
 router.get("/:id", AdminControllers.getSingleAdminByID);
-router.patch("/:id", AdminControllers.updateAdmin);
+router.patch(
+  "/:id",
+  validateRequest(updateDataValidationZodScheam),
+  AdminControllers.updateAdmin
+);
 router.delete("/:id", AdminControllers.deleteAdmin);
 router.delete("/soft/:id", AdminControllers.softDeleteAdminDataByID);
 
